b28ab3f426e1d9234cb220630923eebf5c0f6fde
neilquinn91 <neilquinn91@hotmaill.com>
2014-03-25 12:47:10 +0000

    initial commit

---
+++ src/com/teamtreehouse/ribbit/MarketplaceFragment.java
@@ -0,0 +1,23 @@
+package com.teamtreehouse.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+public class MarketplaceFragment extends Fragment {
+
+@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+			View mView = inflater.inflate(R.layout.fragment_marketplace,container,false);
+
+
+			WebView webView = (WebView) mView.findViewById(R.id.fragment_marketplace_webview);
+			webView.loadUrl("http://www.coniecto.org/WPMU/ill/");
+			return webView;
+	}
+}
